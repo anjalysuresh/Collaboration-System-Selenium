6834f8e1d1e9f4f4289abcfa61895f53da2531ec
Minali24 <minaliupreti24@gmail.com>
2018-07-16 16:46:00 +0530

    Update Feeds_and_notification_for_visible_to_publishable.py

--- Notification-system-Testing-using-selenium-master/Feeds_and_notification_for_visible_to_publishable.py
+++ Notification-system-Testing-using-selenium-master/Feeds_and_notification_for_visible_to_publishable.py
@@ -9,7 +9,7 @@ class signup(unittest.TestCase):
 	def setUp(self):
 		self.driver = webdriver.Remote(command_executor='http://'+'10.129.132.104'+':4444/wd/hub',desired_capabilities=DesiredCapabilities.FIREFOX)#,browser_profile=profile)
 
-	def login(self):
+	def login(self,var):
 		#driver = self.driver
 		driver = webdriver.Remote(command_executor='http://'+'10.129.132.104'+':4444/wd/hub',desired_capabilities=DesiredCapabilities.FIREFOX)#,browser_profile=profile)
 		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT'))
@@ -17,14 +17,14 @@ class signup(unittest.TestCase):
 		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/login/?next=/')
 		elem = driver.find_element_by_id("id_username")
 		user = config('NOTIFICATION_USER').split(',')
-		elem.send_keys(user[0])
+		elem.send_keys(user[var])
 		elem = driver.find_element_by_id("id_password")
 		elem.send_keys(config('NOTIFICATION_PASSWORD'))
 		driver.find_element_by_class_name('btn-block').click()
 
 	def test_draftToVisisbleState(self):
 		driver = webdriver.Remote(command_executor='http://'+'10.129.132.104'+':4444/wd/hub',desired_capabilities=DesiredCapabilities.FIREFOX)#,browser_profile=profile)
-		self.login()
+		self.login(0)
 		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/mydashboard/')
 		driver.find_element_by_xpath('//a [@href="/article-view/' + config('NOTIFICATION_ARTICLE_ID') + '/"]').click()
 		driver.find_element_by_xpath('//a [@href="/article-edit/' + config('NOTIFICATION_ARTICLE_ID') + '/"]').click()
