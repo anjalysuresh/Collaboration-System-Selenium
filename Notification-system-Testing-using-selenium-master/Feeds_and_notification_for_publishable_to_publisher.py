c2d75be6135dbd49374f282e5d0b01f96201a8f6
Minali24 <minaliupreti24@gmail.com>
2018-07-17 16:00:22 +0530

    Update Feeds_and_notification_for_publishable_to_publisher.py

--- Notification-system-Testing-using-selenium-master/Feeds_and_notification_for_publishable_to_publisher.py
+++ Notification-system-Testing-using-selenium-master/Feeds_and_notification_for_publishable_to_publisher.py
@@ -2,13 +2,14 @@ __author__= 'shubh'
 import unittest
 from decouple import config
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class signup(unittest.TestCase):
-	@classmethod
-	def setUpClass(cls):
-		cls.driver = webdriver.Firefox()
+	def setUpClass(self):
+		self.driver = webdriver.Remote(command_executor='http://'+'10.129.132.104'+':4444/wd/hub',desired_capabilities=DesiredCapabilities.FIREFOX)#,browser_profile=profile)
 
-	def login(self,var,driver):
+	def login(self,var):
+		driver = self.driver
 		driver.get("http://" + config('IP_ADDRESS')+ ":" + config('NOTIFICATION_PORT'))
 		driver.find_element_by_xpath('//a [@href="/login/?next=/"]').click()
 		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/login/?next=/')
@@ -19,29 +20,30 @@ class signup(unittest.TestCase):
 		elem.send_keys(config('NOTIFICATION_PASSWORD'))
 		driver.find_element_by_class_name('btn-block').click()
 
-	def checkAllUserNotification(self,var,driver):
-		self.login(var,driver)
+	def checkAllUserNotification(self,var):
+		driver = self.driver
+		self.login(var)
 		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + "/notifications/")
 		driver.implicitly_wait(100)
 		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/logout/')
 
-	def test_draftToVisisbleState(self):	
-		driver = webdriver.Firefox()
-		self.login(2,driver)
+	def test_PublishableToPublishedState(self):	
+		driver = self.driver
+		self.login(1)		# Login by publisher
 		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/communities/')
-		driver.find_element_by_xpath('//a [@href="/community-view/2/"]').click()
-		driver.find_element_by_xpath('//a [@href="/community_content/2/"]').click()
+		driver.find_element_by_xpath('//a [@href="/community-view/' + config('NOTIFICATION_COMMUNITY_ID') + '/"]').click()
+		driver.find_element_by_xpath('//a [@href="/community_content/' + config('NOTIFICATION_COMMUNITY_ID') + '/"]').click()
 		#make the id as visible of the button of visible in html file
-		driver.find_element_by_xpath('//a [@href="/article-view/6/"]').click()
-		driver.find_element_by_xpath('//a [@href="/article-edit/6/"]').click()
+		driver.find_element_by_xpath('//a [@href="/article-view/' + config('NOTIFICATION_ARTICLE_ID') + '/"]').click()
+		driver.find_element_by_xpath('//a [@href="/article-edit/' + config('NOTIFICATION_ARTICLE_ID') + '/"]').click()
 		driver.find_element_by_id('publish').click()
 		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/communities/')
-		driver.find_element_by_xpath('//a [@href="/community-view/2/"]').click()
-		driver.find_element_by_xpath('//a [@href="/community_feed/2/"]').click()
+		driver.find_element_by_xpath('//a [@href="/community-view/' + config('NOTIFICATION_COMMUNITY_ID') + '/"]').click()
+		driver.find_element_by_xpath('//a [@href="/community_feed/' + config('NOTIFICATION_COMMUNITY_ID') + '/"]').click()
 		driver.implicitly_wait(100)
 		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/logout/')
 		for i in range(0,4):
-			self.checkAllUserNotification(i,driver)
+			self.checkAllUserNotification(i)
 
 
 
