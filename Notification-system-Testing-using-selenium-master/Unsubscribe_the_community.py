ad682ed02718df124222580e9411867fec2a3438
Minali24 <minaliupreti24@gmail.com>
2018-07-19 16:12:04 +0530

    Update Unsubscribe_the_community.py

--- Notification-system-Testing-using-selenium-master/Unsubscribe_the_community.py
+++ Notification-system-Testing-using-selenium-master/Unsubscribe_the_community.py
@@ -2,13 +2,15 @@ __author__= 'shubh'
 import unittest
 from decouple import config
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class signup(unittest.TestCase):
-	@classmethod
-	def setUpClass(cls):
-		cls.driver = webdriver.Firefox()
+	
+	def setUp(self):
+		self.driver = webdriver.Remote(command_executor='http://'+'10.129.132.104'+':4444/wd/hub',desired_capabilities=DesiredCapabilities.FIREFOX)#,browser_profile=profile)cls.driver = webdriver.Firefox()
 
-	def login(self,var,driver):
+	def login(self,var):
+		driver = self.driver
 		driver.get("http://" + config('IP_ADDRESS')+ ":" + config('NOTIFICATION_PORT'))
 		driver.find_element_by_xpath('//a [@href="/login/?next=/"]').click()
 		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/login/?next=/')
@@ -20,21 +22,19 @@ class signup(unittest.TestCase):
 		driver.find_element_by_class_name('btn-block').click()
 
 	def test_draftToVisisbleState(self):	
-		driver = webdriver.Firefox()
-		for i in range(1,4):
-			self.login(i,driver)
-			driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/communities/')
-			driver.find_element_by_xpath('//a [@href="/community-view/2/"]').click()
-			driver.find_element_by_id("Unsubscribe").click()
-			driver.find_element_by_id("Yes").click()
-			driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/notifications/')
-			driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/communities/')
-			driver.find_element_by_xpath('//a [@href="/community-view/2/"]').click()
-			driver.find_element_by_xpath('//a [@href="/community_feed/2/"]').click()
-			#make the id as visible of the button of visible in html file
-			driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/logout/')
+		driver = self.driver
+		self.login(4)
+		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/communities/')
+		driver.find_element_by_xpath('//a [@href="/community-view/' + config('NOTIFICATION_COMMUNITY_ID') + '/"]').click()
+		driver.find_element_by_id("Unsubscribe").click()
+		driver.find_element_by_id("Yes").click()
+		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/notifications/')
+		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/communities/')
+		driver.find_element_by_xpath('//a [@href="/community-view/' + config('NOTIFICATION_COMMUNITY_ID') + '/"]').click()
+		driver.find_element_by_xpath('//a [@href="/community_feed/' + config('NOTIFICATION_COMMUNITY_ID') + '/"]').click()
+		#make the id as visible of the button of visible in html file
+		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/logout/')
 			
-	@classmethod
 	def tearDown(cls):
 		cls.driver.quit()
 
