f7930317f2ae82ec707e45110fcd9af89e142d96
Minali24 <minaliupreti24@gmail.com>
2018-07-19 15:35:23 +0530

    Update Subscribe_the_community.py

--- Notification-system-Testing-using-selenium-master/Subscribe_the_community.py
+++ Notification-system-Testing-using-selenium-master/Subscribe_the_community.py
@@ -2,13 +2,14 @@ __author__= 'shubh'
 import unittest
 from decouple import config
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class signup(unittest.TestCase):
-	@classmethod
-	def setUpClass(cls):
-		cls.driver = webdriver.Firefox()
+	
+	def setUp(self):
+		self.driver = webdriver.Remote(command_executor='http://'+'10.129.132.104'+':4444/wd/hub',desired_capabilities=DesiredCapabilities.FIREFOX)#,browser_profile=profile)
 
-	def login(self,var,driver):
+	def login(self,var):
 		driver.get("http://" + config('IP_ADDRESS')+ ":" + config('NOTIFICATION_PORT'))
 		driver.find_element_by_xpath('//a [@href="/login/?next=/"]').click()
 		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/login/?next=/')
@@ -20,14 +21,13 @@ class signup(unittest.TestCase):
 		driver.find_element_by_class_name('btn-block').click()
 
 	def test_draftToVisisbleState(self):	
-		driver = webdriver.Firefox()
-		self.login(3,driver)
+		driver = self.driver
+		self.login(4)
 		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/communities/')
-		driver.find_element_by_xpath('//a [@href="/community-view/2/"]').click()
+		driver.find_element_by_xpath('//a [@href="/community-view/' + config('NOTIFICATION_COMMUNITY_ID') + '/"]').click()
 		driver.find_element_by_id("join-us").click()
 		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/notifications/')
 			
-	@classmethod
 	def tearDown(cls):
 		cls.driver.quit()
 
