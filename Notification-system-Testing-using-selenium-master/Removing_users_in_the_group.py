18f5ef03296b10a3af3955870d4a65eddb5ddc31
Minali24 <minaliupreti24@gmail.com>
2018-07-19 16:54:49 +0530

    Update Removing_users_in_the_group.py

--- Notification-system-Testing-using-selenium-master/Removing_users_in_the_group.py
+++ Notification-system-Testing-using-selenium-master/Removing_users_in_the_group.py
@@ -27,15 +27,17 @@ class signup(unittest.TestCase):
 		elem = driver.find_element_by_id("username")
 		user = config('NOTIFICATION_USER').split(',')
 		elem.send_keys(user[var])
+		print ("removing user: " + user[var])
 		driver.find_element_by_id('remove').click()
 		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/logout/')
 		self.login(var)
 		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/notifications/')
+		print ("notified")
 		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/logout/')
 
 	def test_draftToVisisbleState(self):	
 		driver = self.driver
-		for i in range(1,4):
+		for i in range(1,3):
 			self.login(0)
 			driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/communities/')
 			driver.find_element_by_xpath('//a [@href="/community-view/' + config('NOTIFICATION_COMMUNITY_ID') + '/"]').click()
