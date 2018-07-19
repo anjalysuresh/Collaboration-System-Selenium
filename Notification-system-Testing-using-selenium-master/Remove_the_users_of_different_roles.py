9849ca93c6bc9a56f553639c08d446525f869726
Minali24 <minaliupreti24@gmail.com>
2018-07-19 17:11:35 +0530

    Update Remove_the_users_of_different_roles.py

--- Notification-system-Testing-using-selenium-master/Remove_the_users_of_different_roles.py
+++ Notification-system-Testing-using-selenium-master/Remove_the_users_of_different_roles.py
@@ -29,14 +29,14 @@ class signup(unittest.TestCase):
 		elem.send_keys(user[var])
 		driver.find_element_by_id('remove').click()
 		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/logout/')
-		self.login(var,driver)
+		self.login(var)
 		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/notifications/')
 		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/logout/')
 
 	def test_draftToVisisbleState(self):	
 		driver = self.driver
-		for i in range(1,4):
-			self.login(0)
+		for i in range(1,3):
+			self.login(3)
 			driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/communities/')
 			driver.find_element_by_xpath('//a [@href="/community-view/' + config('NOTIFICATION_COMMUNITY_ID') + '/"]').click()
 			driver.find_element_by_xpath('//a [@href="/community_feed/' + config('NOTIFICATION_COMMUNITY_ID') + '/"]').click()
