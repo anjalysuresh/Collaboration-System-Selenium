5e195bb4d0313c1c28e2bb0f5adec2193aef3a82
Minali24 <minaliupreti24@gmail.com>
2018-07-20 16:04:06 +0530

    Create community and create users for automation

---
+++ Notification-system-Testing-using-selenium-master/create_community.py
@@ -0,0 +1,52 @@
+__author__= 'Minali and Urmi'
+import unittest
+from decouple import config
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+class create_env(unittest.TestCase):
+
+	def setUp(self):
+		self.driver = webdriver.Remote(command_executor='http://'+config('DOCKER_IP')+':'+config('DOCKER_PORT')+'/wd/hub',desired_capabilities=DesiredCapabilities.FIREFOX)#,browser_profile=profile)
+		#self.driver = webdriver.Firefox()
+
+
+	def test_create_community(self):
+		driver = self.driver
+		# Logging in by admin
+		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT')+"/login")
+		elem = driver.find_element_by_id("id_username")
+		user = config('NOTIFICATION_USER').split(',')
+		elem.send_keys(user[3])
+		elem = driver.find_element_by_id("id_password")
+		elem.send_keys(config('NOTIFICATION_PASSWORD'))
+		driver.find_element_by_class_name('btn-block').click()
+		# Creating community
+		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT')+"/create_community")
+		name ="Test Community"
+		tag_line= "only meant for testing"
+		description= "only meant for testing"
+		category= "testing"
+		username= "admin"
+		elem = driver.find_element_by_id("name")
+		elem.send_keys(name)
+		elem = driver.find_element_by_id("tag_line")
+		elem.send_keys(tag_line)
+		elem = driver.find_element_by_name("desc")
+		elem.send_keys(description)
+		elem = driver.find_element_by_id("category")
+		elem.send_keys(category)
+		elem = driver.find_element_by_id("username")
+		elem.send_keys(username)
+		element = driver.find_element_by_id("community_image")
+		element.send_keys("/var/jenkins_home/workspace/Notification_Selenium/Notification-system-Testing-using-selenium-master/community.jpg")
+		element =driver.find_element_by_id("create")
+		element.click()
+
+	def tearDown(self):
+		self.driver.quit()
+
+
+if __name__ == '__main__':
+	unittest.main()
+
