dfea467f0cf99331f4e4fd6ead8b9007a22f108c
Minali24 <minaliupreti24@gmail.com>
2018-07-24 17:16:08 +0530

    Update create_article.py

--- Notification-system-Testing-using-selenium-master/create_article.py
+++ Notification-system-Testing-using-selenium-master/create_article.py
@@ -3,6 +3,7 @@ import unittest
 from decouple import config
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import time
 
 class create_env(unittest.TestCase):
 
@@ -25,14 +26,19 @@ class create_env(unittest.TestCase):
 		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/communities/')
 		print (driver.current_url)
 		driver.find_element_by_xpath('//a [@href="/community-view/' + config('NOTIFICATION_COMMUNITY_ID') + '/"]').click()
-		driver.find_element_by_id("create_resource_id").click()
+		#driver.find_element_by_id("create_resource_id").click()
+		#time.sleep(5)
+		#driver.find_element_by_xpath("//button [@type='button' and @data-target='#modalCreate']").click()
 		driver.find_element_by_xpath("//button [@type='button' and @data-target='#modalCreate']").click()
+		driver.find_element_by_xpath("//button [@type='button' and @data-target='#modalCreateArticle']").click()
 		driver.find_element_by_id("exampleCheck1").click()
 		driver.find_element_by_id("articleCreate").click()
-		elem = driver.find_element_by_id("id_title")
-		elem.send_keys("Test Article a")
+		title = driver.find_element_by_id("title")
+		title.send_keys("TESTING")
 		driver.find_element_by_id("next1").click()
+		time.sleep(10)
 		driver.find_element_by_id("next2").click()
+		time.sleep(10)
 		driver.find_element_by_id("finish").click()
 		
 
@@ -44,4 +50,3 @@ class create_env(unittest.TestCase):
 
 if __name__ == '__main__':
 	unittest.main()
-
