f34ae9b09b88b385cc9205a15d71bf89a0ef5cb2
Minali24 <minaliupreti24@gmail.com>
2018-07-24 16:43:03 +0530

    Add files via upload

---
+++ Notification-system-Testing-using-selenium-master/create_article.py
@@ -0,0 +1,47 @@
+__author__= 'Minali and Urmi'
+import unittest
+from decouple import config
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+class create_env(unittest.TestCase):
+
+	def setUp(self):
+		self.driver = webdriver.Remote(command_executor='http://'+config('DOCKER_IP')+':'+config('DOCKER_PORT')+'/wd/hub',desired_capabilities=DesiredCapabilities.FIREFOX)#,browser_profile=profile)
+		#self.driver = webdriver.Firefox()
+
+
+	def test_create_community(self):
+		driver = self.driver
+		# Logging in by admin
+		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT')+"/login")
+		elem = driver.find_element_by_id("id_username")
+		user = config('NOTIFICATION_USER').split(',')
+		elem.send_keys(user[0])
+		elem = driver.find_element_by_id("id_password")
+		elem.send_keys(config('NOTIFICATION_PASSWORD'))
+		driver.find_element_by_class_name('btn-block').click()
+		# Inside community
+		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/communities/')
+		print (driver.current_url)
+		driver.find_element_by_xpath('//a [@href="/community-view/' + config('NOTIFICATION_COMMUNITY_ID') + '/"]').click()
+		driver.find_element_by_id("create_resource_id").click()
+		driver.find_element_by_xpath("//button [@type='button' and @data-target='#modalCreate']").click()
+		driver.find_element_by_id("exampleCheck1").click()
+		driver.find_element_by_id("articleCreate").click()
+		elem = driver.find_element_by_id("id_title")
+		elem.send_keys("Test Article a")
+		driver.find_element_by_id("next1").click()
+		driver.find_element_by_id("next2").click()
+		driver.find_element_by_id("finish").click()
+		
+
+		
+
+	def tearDown(self):
+		self.driver.quit()
+
+
+if __name__ == '__main__':
+	unittest.main()
+
