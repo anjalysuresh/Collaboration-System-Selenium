cd1d237faaf93afa3d8b3d70781d1b704e273449
Minali24 <minaliupreti24@gmail.com>
2018-07-26 16:28:52 +0530

    Update Group_edit_private_articles_.py

--- Notification-system-Testing-using-selenium-master/Group_edit_private_articles_.py
+++ Notification-system-Testing-using-selenium-master/Group_edit_private_articles_.py
@@ -2,13 +2,15 @@ __author__= 'shubh'
 import unittest
 from decouple import config
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import time
 
 class signup(unittest.TestCase):
-	@classmethod
-	def setUpClass(cls):
-		cls.driver = webdriver.Firefox()
+	def setUp(self):
+		self.driver = webdriver.Remote(command_executor='http://'+'10.129.132.104'+':4444/wd/hub',desired_capabilities=DesiredCapabilities.FIREFOX)#,browser_profile=profile)
 
-	def login(self,var,driver):
+	def login(self,var):
+		driver = self.driver
 		driver.get("http://" + config('IP_ADDRESS')+ ":" + config('NOTIFICATION_PORT'))
 		driver.find_element_by_xpath('//a [@href="/login/?next=/"]').click()
 		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/login/?next=/')
@@ -20,25 +22,27 @@ class signup(unittest.TestCase):
 		driver.find_element_by_class_name('btn-block').click()
 
 	def test_draftToVisisbleState(self):	
-		driver = webdriver.Firefox()
+		driver = self.driver
 		for i in range(1,4):
-			self.login(i,driver)
+			self.login(i)
 			driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/communities/')
-			driver.find_element_by_xpath('//a [@href="/community-view/2/"]').click()
-			driver.find_element_by_xpath('//a [@href="/group-view/1/"]').click()
-			driver.find_element_by_xpath('//a [@href="/group_content/1/"]').click()
-			driver.find_element_by_xpath('//a [@href="/article-view/11/"]').click()
-			driver.find_element_by_xpath('//a [@href="/article-edit/11/"]').click()
+			driver.find_element_by_xpath('//a [@href="/community-view/' + config('NOTIFICATION_COMMUNITY_ID') + '/"]').click()
+			driver.find_element_by_xpath('//a [@href="/group-view/' + config('NOTIFICATION_GROUP_ID') + '/"]').click()
+			driver.find_element_by_xpath('//a [@href="/group_content/' + config('NOTIFICATION_GROUP_ID') + '/"]').click()
+			print (driver.current_url)
+			time.sleep(5)
+			driver.find_element_by_xpath('//a [@href="/article-view/' + config('NOTIFICATION_GROUP_ARTICLE_ID') + '/"]').click()
+			driver.find_element_by_xpath('//a [@href="/article-edit/' + config('NOTIFICATION_GROUP_ARTICLE_ID') + '/"]').click()
+			print (driver.current_url)
+			time.sleep(5)
 			driver.find_element_by_id('savechanges').click()
 			driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/logout/')
 			driver.get("http://" +config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT'))
-			self.login(0,driver)
+			self.login(0)
 			driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/notifications/')
-			driver.implicitly_wait(100)
+			time.sleep(5)
 			driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/logout/')
 
-
-	@classmethod
 	def tearDown(cls):
 		cls.driver.quit()
 
