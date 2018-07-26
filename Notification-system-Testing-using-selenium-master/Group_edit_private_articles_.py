f4018626b9df9980e68da7ff45ebd687b12787fa
Minali24 <minaliupreti24@gmail.com>
2018-07-26 16:37:59 +0530

    Update Group_edit_private_articles_.py

--- Notification-system-Testing-using-selenium-master/Group_edit_private_articles_.py
+++ Notification-system-Testing-using-selenium-master/Group_edit_private_articles_.py
@@ -23,7 +23,7 @@ class signup(unittest.TestCase):
 
 	def test_draftToVisisbleState(self):	
 		driver = self.driver
-		for i in range(1,4):
+		for i in range(1,3):
 			self.login(i)
 			driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/communities/')
 			driver.find_element_by_xpath('//a [@href="/community-view/' + config('NOTIFICATION_COMMUNITY_ID') + '/"]').click()
