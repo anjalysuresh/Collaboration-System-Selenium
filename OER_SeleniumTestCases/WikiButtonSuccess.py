df8366a7a9a7e7f29f118a155e12a6b3030452bc
Minali24 <minaliupreti24@gmail.com>
2018-07-31 16:54:44 +0530

    Update WikiButtonSuccess.py

--- OER_SeleniumTestCases/WikiButtonSuccess.py
+++ OER_SeleniumTestCases/WikiButtonSuccess.py
@@ -9,28 +9,28 @@ import time
 
 class NavBarActions(unittest.TestCase):
 
-    def setUp(self):
-	self.driver = webdriver.Remote(command_executor='http://'+config('DOCKER_IP')+':'+config('DOCKER_PORT')+'/wd/hub',desired_capabilities=DesiredCapabilities.FIREFOX)#,browser_profile=profile)
+	def setUp(self):
+		self.driver = webdriver.Remote(command_executor='http://'+config('DOCKER_IP')+':'+config('DOCKER_PORT')+'/wd/hub',desired_capabilities=DesiredCapabilities.FIREFOX)#,browser_profile=profile)
 
-    def test_navbar_wiki(self):
-	driver = self.driver
-	# Logging in by admin
-	driver.get("http://" + config('IP_ADDRESS') + ":" + config('CONTENT_TOOLS_PORT')+"/login")
-	elem = driver.find_element_by_id("id_username")
-	user = config('CONTENT_TOOLS_USER').split(",")
-	elem.send_keys(user[3])
-	elem = driver.find_element_by_id("id_password")
-	elem.send_keys(config('CONTENT_TOOLS_PASSWORD'))
-	driver.find_element_by_class_name('btn-block').click()
-	driver.find_element_by_xpath('//a[@href="/communities/"]').click()
-	driver.find_element_by_xpath('//a [@href="/community-view/' + config('CONTENT_TOOLS_COMMUNITY_ID') + '/"]').click()
-	community_name = driver.find_element_by_xpath('//li [@class="breadcrumb-item active"]').text
-	driver.find_element_by_xpath('//a[@href="/wiki/' + community_name + config('CONTENT_TOOLS_COMMUNITY_ID') + '"]').click()
-	print (driver.current_url)
+	def test_navbar_wiki(self):
+		driver = self.driver
+		# Logging in by admin
+		driver.get("http://" + config('IP_ADDRESS') + ":" + config('CONTENT_TOOLS_PORT')+"/login")
+		elem = driver.find_element_by_id("id_username")
+		user = config('CONTENT_TOOLS_USER').split(",")
+		elem.send_keys(user[3])
+		elem = driver.find_element_by_id("id_password")
+		elem.send_keys(config('CONTENT_TOOLS_PASSWORD'))
+		driver.find_element_by_class_name('btn-block').click()
+		driver.find_element_by_xpath('//a[@href="/communities/"]').click()
+		driver.find_element_by_xpath('//a [@href="/community-view/' + config('CONTENT_TOOLS_COMMUNITY_ID') + '/"]').click()
+		community_name = driver.find_element_by_xpath('//li [@class="breadcrumb-item active"]').text
+		driver.find_element_by_xpath('//a[@href="/wiki/' + community_name + config('CONTENT_TOOLS_COMMUNITY_ID') + '"]').click()
+		print (driver.current_url)
 
 
-    def tearDown(self):
-        self.driver.quit()
+	def tearDown(self):
+        	self.driver.quit()
 
 if __name__ == '__main__':
-        unittest.main()
+	unittest.main()
