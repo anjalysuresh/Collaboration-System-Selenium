8ddfd35460a79c83895e337ddcc8f68feeb11e86
Minali24 <minaliupreti24@gmail.com>
2018-07-31 16:48:48 +0530

    Update WikiButtonSuccess.py

--- OER_SeleniumTestCases/Wiki Testing/WikiButtonSuccess.py
+++ OER_SeleniumTestCases/Wiki Testing/WikiButtonSuccess.py
@@ -4,26 +4,29 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 import os
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import time
 
 class NavBarActions(unittest.TestCase):
 
     def setUp(self):
-	self.driver = webdriver.Firefox()
-	self.driver.maximize_window()
+	self.driver = webdriver.Remote(command_executor='http://'+config('DOCKER_IP')+':'+config('DOCKER_PORT')+'/wd/hub',desired_capabilities=DesiredCapabilities.FIREFOX)#,browser_profile=profile)
 
     def test_navbar_wiki(self):
-	self.driver.get("http://172.17.0.1:7000/login/")
-	user ="root"
-        pwd= "root1234"
-        elem = self.driver.find_element_by_id("id_username")
-        elem.send_keys(user)
-        elem = self.driver.find_element_by_id("id_password")
-        elem.send_keys(pwd)
-        self.driver.find_element_by_class_name('btn-block').click()
-	self.driver.find_element_by_xpath('//a[@href="/communities/"]').click()
-	self.driver.get("http://172.17.0.1:7000/community-view/1/")
-	self.driver.find_element_by_xpath('//a[@href="/wiki/IIT Bombay1"]').click()
-	self.driver.find_element_by_class_name("container")
+	driver = self.driver
+	# Logging in by admin
+	driver.get("http://" + config('IP_ADDRESS') + ":" + config('CONTENT_TOOLS_PORT')+"/login")
+	elem = driver.find_element_by_id("id_username")
+	user = config('CONTENT_TOOLS_USER').split(",")
+	elem.send_keys(user[3])
+	elem = driver.find_element_by_id("id_password")
+	elem.send_keys(config('CONTENT_TOOLS_PASSWORD'))
+	driver.find_element_by_class_name('btn-block').click()
+	driver.find_element_by_xpath('//a[@href="/communities/"]').click()
+	driver.find_element_by_xpath('//a [@href="/community-view/' + config('CONTENT_TOOLS_COMMUNITY_ID') + '/"]').click()
+	community_name = driver.find_element_by_xpath('//li [@class="breadcrumb-item active"]').text
+	driver.find_element_by_xpath('//a[@href="/wiki/' + community_name + config('CONTENT_TOOLS_COMMUNITY_ID') + '"]').click()
+	print (driver.current_url)
 
 
     def tearDown(self):
