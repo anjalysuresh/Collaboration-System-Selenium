edee91894ada2ce37c89caaa8a2b0801d4010a5c
Minali24 <minaliupreti24@gmail.com>
2018-08-01 15:38:19 +0530

    Update DeleteArticle.py

--- OER_SeleniumTestCases/DeleteArticle.py
+++ OER_SeleniumTestCases/DeleteArticle.py
@@ -11,7 +11,7 @@ import time
 class contenttools(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Remote(command_executor='http://'+config('DOCKER_IP')+':'+config('DOCKER_PORT')+'/wd/hub',desired_capabilities=DesiredCapabilities.FIREFOX)#,browser_profile=profile)
+      self.driver = webdriver.Remote(command_executor='http://'+config('DOCKER_IP')+':'+config('DOCKER_PORT')+'/wd/hub',desired_capabilities=DesiredCapabilities.FIREFOX)#,browser_profile=profile)
 
     def login(self,var):
       driver = self.driver
@@ -25,18 +25,18 @@ class contenttools(unittest.TestCase):
       elem.send_keys(config('NOTIFICATION_PASSWORD'))
       driver.find_element_by_class_name('btn-block').click()
       
-    def test_draftToVisisbleState(self):	
-		  driver = self.driver
-		  self.login(3) # logging in with tester who is the creater of the article
-			driver.get("http://" + config('IP_ADDRESS') + ":" + config('CONTENT_TOOLS_PORT') + '/mydashboard/')
-			driver.find_element_by_xpath('//a [@href="/article-view/' + config('CONTENT_TOOLS_ARTICLE_ID') + '/"]').click()
-			driver.find_element_by_xpath('//a [@href="/article-edit/' + config('CONTENT_TOOLS_ARTICLE_ID') + '/"]').click()
+    def test_draftToVisisbleState(self):
+      driver = self.driver
+      self.login(3) # logging in with tester who is the creater of the article
+      driver.get("http://" + config('IP_ADDRESS') + ":" + config('CONTENT_TOOLS_PORT') + '/mydashboard/')
+      driver.find_element_by_xpath('//a [@href="/article-view/' + config('CONTENT_TOOLS_ARTICLE_ID') + '/"]').click()
+      driver.find_element_by_xpath('//a [@href="/article-edit/' + config('CONTENT_TOOLS_ARTICLE_ID') + '/"]').click()
       driver.find_element_by_xpath('//a [@href="/article-delete/' + config('CONTENT_TOOLS_ARTICLE_ID') + '/"]').click()
-			print (driver.current_url)
+      print (driver.current_url)
       driver.find_element_by_class_name('btn-danger').click()
 
     def tearDown(self):
-        self.driver.quit()
+      self.driver.quit()
 
 
 if __name__ == '__main__':
