2ae48eb8c401e62842cae106164c742810aa923c
Minali24 <minaliupreti24@gmail.com>
2018-08-03 17:54:08 +0530

    Update H5P_content_dropdown_check.py

--- OER_SeleniumTestCases/H5P_content_dropdown_check.py
+++ OER_SeleniumTestCases/H5P_content_dropdown_check.py
@@ -27,7 +27,7 @@ class create_env(unittest.TestCase):
 		print (driver.current_url)
 		driver.find_element_by_xpath('//a [@href="/community-view/' + config('CONTENT_TOOLS_COMMUNITY_ID') + '/"]').click()
 		#driver.find_element_by_id("create_resource_id").click()
-		#time.sleep(5)
+		time.sleep(5)
 		#driver.find_element_by_xpath("//button [@type='button' and @data-target='#modalCreate']").click()
 		driver.find_element_by_xpath("//button [@type='button' and @data-target='#modalCreate']").click()
 		driver.find_element_by_xpath("//button [@type='button' and @data-target='#modalCreateh5p']").click()
@@ -35,12 +35,12 @@ class create_env(unittest.TestCase):
 		driver.find_element_by_id("Yes").click()
 		title = driver.find_element_by_id("id_title")
 		title.send_keys("TEST h5p Content Tools abc")
-		driver.find_element_by_id("id_h5p_type_0").click()
-		time.sleep(10)
-		driver.find_element_by_id("id_h5p_type_1").click()
-		time.sleep(10)
+		driver.find_element_by_xpath("//label [@for='id_h5p_type_0']").click()
+		time.sleep(5)
+		driver.find_element_by_xpath("//label [@for='id_h5p_type_1']").click()
+		time.sleep(5)
 		driver.find_element_by_id("submit_editor").click()
-    driver.find_element_by_id("h5peditor-library").click()
+		driver.find_element_by_id("h5peditor-library").click()
 		
 		
 
