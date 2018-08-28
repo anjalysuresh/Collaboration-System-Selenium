5c0fcd2311696a8731ca63a906910a6ddc0e9adc
anjalysuresh <anjalysuresh@yahoo.co.in>
2018-08-28 12:53:11 +0530

    Update create_users.py

--- Notification-system-Testing-using-selenium-master/create_users.py
+++ Notification-system-Testing-using-selenium-master/create_users.py
@@ -8,8 +8,8 @@ class create_env(unittest.TestCase):
 
 	def setUp(self):
 		cap= DesiredCapabilities().FIREFOX
-        cap["marionette"]= True
-        self.driver = webdriver.Remote(command_executor='http://'+config('DOCKER_IP')+':'+config('DOCKER_PORT')+'/wd/hub',desired_capabilities=DesiredCapabilities.FIREFOX)
+                cap["marionette"]= True
+                self.driver = webdriver.Remote(command_executor='http://'+config('DOCKER_IP')+':'+config('DOCKER_PORT')+'/wd/hub',desired_capabilities=DesiredCapabilities.FIREFOX)
 
 
 	def create_user(self,username,email,password):
